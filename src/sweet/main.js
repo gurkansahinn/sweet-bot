35de48cd5fff6412d6a005a522dfd65df926da0b
Gürkan Şahin <60393263+liilamper@users.noreply.github.com>
2020-12-11 18:17:17 +0300

    kanala mesaj göndermesine gerek yok

--- src/sweet/main.js
+++ src/sweet/main.js
@@ -63,7 +63,6 @@ client.on("message", async (message) => {
         client.commands.get(command).execute(message, args);
     } catch (error) {
         console.error(error);
-        message.reply('there was an error trying to execute that command!');
     }
 });
 
