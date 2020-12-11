eacf723e9a9e126e925970c64e93a4509a4a4472
Gürkan Şahin <60393263+liilamper@users.noreply.github.com>
2020-12-11 03:02:48 +0300

    AoC API'ı kanala mesaj olarak bilgi gönderiyor.

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 Sağ üst tarafta bulunan Fork butonuna tıklayın ve kendi github profilinizde bir depo oluşturmasına izin verin. Github Desktop veya Git CLI üzerinden bu depoyu klonlayın ardından içeriğini yapılacaklar listesine göre veya kendi fikirlerinize göre kodlayın ardından kendi deponuza pushlayın ve github profilinizde bulunan depo sayfasına girerek Pull Request sekmesine tıklayın. Geçerli başlığı ve açıklamayı yazarak bir pull request isteği yaratın ve bekleyin.
 
 # Yapılacaklar Listesi
-- Advent Of Code API'ı ile tam zamanlı listeye erişim.
++ Advent Of Code API'ı ile tam zamanlı listeye erişim. (yapıldı)
 -
 -
--- src/sweet.js
+++ src/sweet.js
@@ -1,6 +1,10 @@
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
 const Discord = require('discord.js');
+
+var sweet = null;
 const client = new Discord.Client();
-let sweet = null;
+dotenv.config();
 
 function Sweet(token) {
     this.token = token;
@@ -25,12 +29,55 @@ Sweet.prototype.setStatus = function setStatus(statusType, statusText, statusUrl
     this.client.user.setActivity(statusText, { type: "STREAMING", url: statusUrl })
 }
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log(`${client.user.tag} aktif edildi!`);
 });
 
-client.on('message', (msg) => {
+client.on("message", async (message) => {
+    const prefix = "!";
 
+    if (message.author.bot) return;
+    if (!message.guild) return;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "aoc2020") {
+
+        const leaderBoardTemplate = new Discord.MessageEmbed()
+            .setColor(15844367)
+            .setURL('https://adventofcode.com')
+            .setDescription('Advent Of Code 2020 - Liderlik Tablosu')
+            .setThumbnail('https://repository-images.githubusercontent.com/317076987/2058f880-327b-11eb-9a91-71903b2d5120')
+            .setTimestamp();
+
+        const response = await getLeaderBoardData("https://adventofcode.com/2020/leaderboard/private/view/1156609.json");
+
+        var leaderBoardObject = JSON.parse(response);
+
+        const membersObject = leaderBoardObject.members;
+        let members = Object.getOwnPropertyNames(membersObject).map(key => membersObject[key]) || [];
+
+        members.forEach((object, index) => {
+            leaderBoardTemplate.addField(members[index].name, members[index].local_score + "puan ve " + members[index].stars + " adet yıldıza sahip.");
+        });
+        message.channel.send(leaderBoardTemplate);
+    }
 });
+
+async function getLeaderBoardData(url = '') {
+
+    const cookieData = {
+        headers: {
+            cookie: '_ga=' + process.env._ga + ' _gid=' + process.env._gid + ' session=' + process.env.SESSION
+        }
+    };
+
+    const response = await fetch(url, cookieData);
+    const responseText = await (response).text();
+    return responseText;
+}
 
 module.exports = Sweet;
